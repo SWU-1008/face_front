f51eb0a2b06597188e8c8106abad31970ea3bdcb
pengj <pengjianbiao@hotmail.com>
2019-12-12 16:36:18 +0800

    搞定了 3D Three.js 的 MTL 和 OBJ 的 loader，非常开心的事情啊

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,12 +1,108 @@
 <template>
-  <div class="home">
+  <div>
     <img alt="Vue logo" src="../../assets/logo.png">
+    <div id="mycontainer"></div>
   </div>
 </template>
-
 <script>
+import * as Three from 'three' // 导入three.js
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js' // 导入OBJLoader 读取obj模型
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js' // 导入MTLLoader 读取mtl材质
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js' // 使用OrbitControls控制模型的旋转,放大,缩小
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      light: null,
+      controls: null
+    }
+  },
+  methods: {
+    // 外部模型加载函数
+    loadObj () {
+      console.log('loadObj')
+      let that = this
+      // 材质读取,包含材质
+      new MTLLoader().load('/head3d/head3d.obj.mtl', materials => {
+        materials.preload()
+        // 模型读取
+        new OBJLoader().setMaterials(materials).load('/head3d/head3d.obj', obj => {
+          // 更改模型的尺寸
+          obj.scale.set(5, 5, 5)
+          obj.position.set(0, 0, 0)
+          // 在场景中加入坐标系
+          let axes = new Three.AxesHelper(100)
+          that.scene.add(obj)
+          that.scene.add(axes)
+        }, function (xhr) {
+          // onProgress回调
+          console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+        }, function (err) {
+          // onError回调
+          console.log(err)
+        })
+      })
+    },
+    init: function () {
+      console.log('init')
+      this.scene = new Three.Scene()
+      this.scene.add(new Three.AmbientLight(0x999999))// 环境光
+      this.light = new Three.DirectionalLight(0xdfebff, 0.45)// 从正上方（不是位置）照射过来的平行光，0.45的强度
+      this.light.position.set(0, 2800, 2800)
+      this.light.position.multiplyScalar(0.3)
+      this.scene.add(this.light)
+      // 初始化相机
+      console.log(window.innerWidth)
+      console.log(window.innerHeight)
+      this.camera = new Three.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 2800)
+      this.camera.position.set(0, 0, 2800)
+      this.camera.lookAt(this.scene.position)
+      // 渲染模型 alpha为透明背景
+      this.renderer = new Three.WebGLRenderer({
+        alpha: true
+      })
+      // this.renderer.setClearColor(0x000000)
+      this.renderer.setPixelRatio(window.devicePixelRatio)// 为了兼容高清屏幕
+      this.renderer.setSize(window.innerWidth, window.innerHeight)
+      // 初始化控制器
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+      this.controls.target.set(0, 0, 100)
+      this.controls.minDistance = 1500
+      this.controls.maxDistance = 2800
+      this.controls.maxPolarAngle = Math.PI / 2
+      this.controls.update()
+      console.log('init111')
+      const container = document.getElementById('mycontainer')
+      console.log('init222222')
+      console.log(container)
+      container.appendChild(this.renderer.domElement)
 
+      // this.renderer = new Three.WebGLRenderer({
+      //   antialias: true,
+      //   alpha: true // 默认情况下为黑色场景，此处设置为透明（即白色场景）
+      // })
+      console.log('init after')
+    },
+    animate: function () {
+      console.log('animate')
+      requestAnimationFrame(this.animate)
+      this.renderer.render(this.scene, this.camera)
+    }
+  },
+  mounted () {
+    this.init()
+    this.loadObj()
+    this.animate()
+  }
 }
 </script>
+<style scoped>
+  #mycontainer {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  }
+</style>
